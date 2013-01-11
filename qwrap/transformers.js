0f266395c6fc7fe64004f86340c0877dbd70990c
Hex <hex@codeigniter.tk>
2013-01-11 20:48:43 +0800

    增加跨浏览器 hashchange 支持

--- qwrap/transformers.js
+++ qwrap/transformers.js
@@ -64,8 +64,8 @@ Transformers for QWrap 核心库
 
             return [].slice.call(source);
         }
-
     };
+
 
     TF.Library.Hash = function(object) {
         this.data = object || {};
@@ -157,7 +157,192 @@ Transformers for QWrap 核心库
     });
 
 
-    // 组件类
+
+
+
+    TF.Library.LocationHash = function(options) {
+        this.options = {
+            blank_page: 'javascript:0',
+            start: false
+        };
+        this.iframe = null;
+        this.handle = false;
+        this.useIframe = (Browser.ie && (typeof(document.documentMode)=='undefined' || document.documentMode < 8));
+
+        this.count = history.length;
+        this.states = [];
+        this.states[this.count] = this.getHash();
+        this.state = null;
+        this.supports = (('onhashchange' in window) && (typeof(document.documentMode) == 'undefined' || document.documentMode > 7));
+
+        mix(this.options, options, true);
+
+        CustEvent.createEvents(this, ['hashChanged']);
+
+        this.initialize();
+
+        return this;
+    };
+
+    mix(TF.Library.LocationHash.prototype, {
+        initialize: function(){
+            var self = this;
+
+            // Disable Opera's fast back/forward navigation mode
+            if (Browser.opera && window.history.navigationMode) {
+                window.history.navigationMode = 'compatible';
+            }
+
+             // IE8 in IE7 mode defines window.onhashchange, but never fires it...
+            if (this.supports) {
+                // The HTML5 way of handling DHTML history...
+                W(window).on('hashchange', function() {
+                    var hash = self.getHash();
+                    if (hash == self.currentHash) {
+                        return;
+                    }
+                    self.fire('hashChanged', {hash: hash});
+                });
+            }
+            else {
+                if (this.useIframe) {
+                    this.initializeHistoryIframe();
+                }
+
+                if (this.options.start) {
+                    this.start();
+                }
+            }
+
+        },
+
+        initializeHistoryIframe: function() {
+            var hash = this.getHash(), doc;
+            this.iframe = W(Dom.createElement('iframe', {
+                src: this.options.blank_page
+            })).css({
+                'position'  : 'absolute',
+                'top'       : 0,
+                'left'      : 0,
+                'width'     : '1px',
+                'height'    : '1px',
+                'visibility': 'hidden'
+            }).appendTo(W('body')[0])[0];
+
+            this.istate = null;
+
+            doc = (this.iframe.contentDocument) ? this.iframe.contentDocument  : this.iframe.contentWindow.document;
+            doc.open();
+            doc.write('<html><head><title>' + document.title + '</title></head><body id="state">' + hash + '</body></html>');
+            doc.close();
+            this.istateOld = false;
+        },
+
+        checkHash: function(){
+            if (this.timeout) return;
+            var state = this.getState();
+            if (this.state == state) return;
+            if (Browser.ie && (this.state !== null)) {
+                this.setState(state, true);
+            }
+            else {
+                this.state = state;
+            }
+
+            this.fire('hashChanged', {hash: state});
+        },
+
+        getState: function() {
+            var state = this.getHash();
+            if (this.iframe) {
+                var doc = this.iframe.contentWindow.document;
+                if (doc && doc.body.id == 'state') {
+                    var istate = doc.body.innerText;
+                    if (this.state == state) return istate;
+                    this.istateOld = true;
+                }
+                else {
+                    return this.istate;
+                }
+            }
+
+            return state;
+        },
+
+        getHash: function() {
+            var href = decodeURI(top.location.href);
+            var pos = href.indexOf('#') + 1;
+            return (pos) ? href.substr(pos) : '';
+        },
+
+        pick: function() {
+            for (var i = 0, l = arguments.length; i < l; i++){
+                if (arguments[i] != undefined) return arguments[i];
+            }
+            return null;
+        },
+
+        setState: function(state, fix) {
+            state = this.pick(state, '');
+            top.location.hash = state || '#';
+
+            if (Browser.ie && (!fix || this.istateOld)) {
+                if (!this.iframe) {
+                    this.iframe = W(Dom.createElement('iframe', {
+                        src: this.options.blank_page
+                    })).css({
+                        'position'  : 'absolute',
+                        'top'       : 0,
+                        'left'      : 0,
+                        'width'     : '1px',
+                        'height'    : '1px',
+                        'visibility': 'hidden'
+                    }).appendTo(document.body)[0];
+
+                    this.istate = this.state;
+                }
+
+                var doc = (this.iframe.contentDocument) ? this.iframe.contentDocumnet  : this.iframe.contentWindow.document;
+                doc.open();
+                doc.write('<html><head><title>' + document.title + '</title></head><body id="state">' + state + '</body></html>');
+                doc.close();
+                this.istateOld = false;
+            }
+
+            this.state = state;
+        },
+
+//        observeTimeout: function() {
+//            if (this.timeout) this.timeout = this.clear(this.timeout);
+//            else this.timeout = setTimeout(bind(this.observeTimeout, this), 200);
+//        },
+
+        clear: function(timer) {
+            clearTimeout(timer);
+            clearInterval(timer);
+            return null;
+        },
+
+        start: function() {
+            if (this.supports) {
+                this.fire('hashChanged', {hash: this.getHash()});
+                return;
+            }
+
+            this.handle = setInterval(bind(this.checkHash, this), 200);
+            this.started = true;
+
+            return this;
+        },
+
+        stop: function() {
+            this.clear(this.handle);
+        }
+    });
+
+
+
+    // 组件加载器类
 
     var decamelize = function(s) {
         return s.replace(/[A-Z]/g, function(a) {
@@ -290,14 +475,35 @@ Transformers for QWrap 核心库
     // 组件系统内部方法
     var componentSys = {
         // 组件消息处理中心
-        post: function(msg, args) {
+        postMessage: function(msg, args) {
             // 如果组件还未渲染，则等待组件完成渲染，再传递最后一个消息
             if (!this.rendered) {
-                this.lastMessage = {msg: msg, args: args};
-                this.render();
+                // 系统事件预处理
+                switch (msg) {
+                    case 'component-show':
+                        this.hidden = false;
+                        break;
+
+                    case 'component-only-show':
+                        this.hidden = false;
+                        break;
+
+                    case 'component-hide':
+                        this.hidden = true;
+                        break;
+                }
+
+                if (msg == 'component-route') {
+                    this.lastRouterMessage = {msg: msg, args: args};
+                }
+                else {
+                    this.lastMessage = {msg: msg, args: args};
+                }
+                if (!this.rendering) {
+                    this.render();
+                }
                 return;
             }
-
             //if (!$defined(args)) args = {};
 
             // before 用于处理消息返回值，表示是否继续传递消息
@@ -314,28 +520,38 @@ Transformers for QWrap 核心库
 
             // 系统事件处理
             switch (msg) {
+                case 'component-route':
+                    if (this.instance && Object.isFunction(this.instance['componentRouteAction'])) {
+                        this.instance['componentRouteAction'].call(this.instance, args);
+                    }
+                    else {
+                        this.route(args);
+                    }
+                    return;
+                    break;
+
                 case 'component-render':
-                    this.render();
+                    this.render(args);
                     break;
 
                 case 'component-refresh':
-                    this.refresh();
+                    this.refresh(args);
                     break;
 
                 case 'component-destroy':
-                    this.destroy();
+                    this.destroy(args);
                     break;
 
                 case 'component-show':
-                    this.show();
+                    this.show(args);
                     break;
 
                 case 'component-only-show':
-                    this.onlyShow();
+                    this.onlyShow(args);
                     break;
 
                 case 'component-hide':
-                    this.hide();
+                    this.hide(args);
                     break;
             }
 
@@ -343,6 +559,10 @@ Transformers for QWrap 核心库
             if (this.instance && Object.isFunction(this.instance[funcName + 'Action'])) {
                 this.instance[funcName + 'Action'].call(this.instance, args);
             }
+        },
+
+        route: function(args) {
+            this.onlyShow(args);
         },
 
         render: function() {
@@ -477,14 +697,21 @@ Transformers for QWrap 核心库
                     this.instance.fire('refreshReady', {instance: this.instance, fullName: this.fullName, name: this.name, channelName: this.channelName});
                 }
                 else {
+                    // 处理事件委托绑定
+                    this.delegateEvent(this.instance.bindEvent);
+
                     this.instance.DomReady();
 
                     // 渲染完毕
                     this.rendered = true;
 
                     // 渲染完毕后要处理未渲染时的最后一条消息
+                    if (this.lastRouterMessage) {
+                        this.postMessage(this.lastRouterMessage.msg, this.lastRouterMessage.args);
+                        this.lastRouterMessage = null;
+                    }
                     if (this.lastMessage) {
-                        this.post(this.lastMessage.msg, this.lastMessage.args);
+                        this.postMessage(this.lastMessage.msg, this.lastMessage.args);
                         this.lastMessage = null;
                     }
 
@@ -498,6 +725,69 @@ Transformers for QWrap 核心库
             this.rendering = false;
 
             return true;
+        },
+
+        delegateEvent: function(configs){
+            var value, test, eventName;
+            for(var key in configs){
+                value = configs[key];
+                test = key.match(/\{(\S+)\}(.+)/);
+                if (test) {
+                    eventName = test[1];
+                    selector = test[2].trim();
+                }
+                else {
+                    eventName = 'click';
+                    selector = key;
+                }
+                if (Object.isFunction(value)) {
+                    this.topElement.delegate(selector, eventName, value);
+                }
+            }
+        },
+
+        // 创建浏览器历史路由规则
+        createRouter: function(routes, fn, scope) {
+            if (routes) {
+                var routerItems = routes || [];
+                var routerFn = fn || function(){};
+                var routerScope = scope || this.instance;
+
+                var func = function(uri) {
+                    var reg, isMatch = false;
+                    routerItems.forEach(function(item, index) {
+                        Object.map(item, function(value, key, obj){
+                            key = key.replace(/:num/gi, '[0-9]+');
+                            key = key.replace(/:any/gi, '.*');
+
+                            reg = new RegExp('^' + key + '$');
+                            if (uri.match(reg)) {
+                                if (value.contains('$') && key.contains('(')) {
+                                    value = uri.replace(reg, value);
+                                }
+                                isMatch = true;
+                                routerFn.apply(routerScope, [value, index, item]);
+                            }
+                        });
+                    });
+
+                    if (!isMatch) {
+                        routerFn.apply(routerScope, [uri, -1, null]);
+                    }
+                }
+
+                //[{'key':'inbox/(:any)/(:any)', 'value': 'inbox/$2'}, ]
+
+                TF.Core.Application.subscribe('GlobalRoute', function(uri) {
+                    func(uri.toLowerCase());
+                }, this);
+
+                // 一般这里会错过首次的全局路由事件，所以要手动执行一次
+                var uri = TF.Core.Router.parseUri();
+                uri = [uri.channelName, uri.name, uri.params];
+                uri = uri[1] ? uri.join('/') : uri[0];
+                func(uri.toLowerCase());
+            }
         },
 
         query: function(selector) {
@@ -549,6 +839,8 @@ Transformers for QWrap 核心库
 
         // 在当前容器下只显示自己
         onlyShow: function() {
+            this.hidden = false;
+
             if (!this.rendered) {
                 return;
             }
@@ -685,6 +977,8 @@ Transformers for QWrap 核心库
                 this.show(item);
             }, exports);
             preloadShow.length = 0;
+            // 广播一个全部组件加载完毕的消息
+            TF.Core.Application.publish('GlobalComponentLoaded');
         };
 
         var exports = {
@@ -800,7 +1094,7 @@ Transformers for QWrap 核心库
 
                         // 传递给组件
                         components.get(item).forEach(function(com) {
-                            com.sys.post(msg, args);
+                            com.sys.postMessage(msg, args);
                         });
                     }
                     else {
@@ -862,6 +1156,115 @@ Transformers for QWrap 核心库
 
         return exports;
     }();
+
+
+    // URI 路由部分
+    TF.Core.Router = function() {
+        var defaultChannelName = 'Home',
+            defaultName = 'Home';
+
+        // 额外的路由规则，不会影响系统既有路由规则
+        var addtionRouter = [];
+
+        var parse = function(hash) {
+            var bits = hash.match(/tf-([^\/]+)\/?([^\/]*)\/?(.*)/);
+            var name, params, channelName;
+            if (bits) {
+                channelName = bits[1].capitalize();
+                name = bits[2] ? bits[2].camelize().capitalize() : defaultName;
+
+                params = bits[3].split('/');
+            }
+            else {
+                channelName = defaultChannelName;
+                name = defaultName;
+                params = [];
+            }
+//alert(bits);
+            return {name: name, channelName: channelName, params: params, uri: exports.makeUri(channelName, name, params)};
+        };
+
+        var callback = function(hash){
+            var result = parse(hash),
+                name = result.name,
+                channelName = result.channelName,
+                params = result.params;
+
+            var fullName = channelName + name;
+
+            // 系统默认路由
+            if (TF.Core.ComponentMgr.has(fullName)) {
+                TF.Core.Application.publish('GlobalRoute', [result.uri, [channelName, name, params]]);
+                TF.Core.ComponentMgr.post(fullName, 'component-route', params);
+            }
+            else {
+                // 非法参数
+                window.location = '#tf-' + defaultChannelName.toLowerCase() + '/' + defaultName.toLowerCase();
+            }
+
+            // 额外路由
+            var reg, isMatch = false;
+            addtionRouter.forEach(function(item, index) {
+                Object.map(item, function(value, key){
+                    key = key.replace(/:num/gi, '[0-9]+');
+                    key = key.replace(/:any/gi, '.*');
+
+                    reg = new RegExp('^' + key + '$');
+                    if (result.uri.match(reg)) {
+                        if (value.contains('$') && key.contains('(')) {
+                            value = result.uri.replace(reg, value);
+                        }
+                        isMatch = true;
+                        // 匹配上
+                        if (TF.Core.ComponentMgr.has(value)) {
+                            TF.Core.Application.publish('GlobalRoute', [result.uri, [channelName, name, params]]);
+                            TF.Core.ComponentMgr.post(value, 'component-route', params);
+                        }
+                    }
+                });
+            });
+
+        };
+
+        var exports = {
+            create: function(router, channelName, name) {
+                addtionRouter = router || addtionRouter;
+                defaultChannelName = channelName || defaultChannelName;
+                defaultName = name || defaultName;
+
+                TF.Core.Application.subscribe('GlobalComponentLoaded', function(){
+                    new TF.Library.LocationHash({start: true}).on('hashChanged', function(e){
+                        //alert(e);
+                        callback(e.hash);
+                    });
+//                    $(window).hashchange(function(){
+//                        callback(location.hash);
+//                    });
+//                    $(window).hashchange();
+                });
+            },
+
+            // 解析 hash 中的字符串，取得里面的信息
+            parseUri: function(uri) {
+                return parse(uri || location.hash);
+            },
+
+            makeUri: function(channelName, name, params) {
+                channelName = channelName || '';
+                name = name || '';
+                params = params || [];
+
+                channelName = channelName.toLowerCase();
+                name = name.decamelize().slice(1);
+                params = params.join('/');
+
+                return [channelName, name, params].join('/');
+            }
+        };
+
+        return exports;
+    }();
+
 
     var appReady = false;
     var appSubscriptions = new TF.Library.Hash();
